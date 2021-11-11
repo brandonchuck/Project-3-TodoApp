90170a3e95876b2400c0219909df912088e63919
Brandon Chuck <blc42310@gmail.com>
2021-11-11 12:32:32 -0800

    Refactored todo.js to abstract logic from addTodo function; Made variables apply to context of app

--- todo.js
+++ todo.js
@@ -1,101 +1,69 @@
-// Grab reference to unordered list for adding li's
-var list = document.querySelector('.items'); 
+document.querySelector('#input-btn')
+  .addEventListener('click', displayTodo);
 
-// Grab reference to input button for adding todos in ul
-var newTodoBtn = document.querySelector('#input-btn');
-newTodoBtn.addEventListener('click', addTodo);
 
-var clearAll = document.querySelector('#clear-btn');
-clearAll.addEventListener('click', clearAllTodos);
+document.querySelector('#clear-btn')
+  .addEventListener('click', clearTodos);
 
-// ----- CREATE NEW TODO ----- 
-function addTodo(){
 
-  // grab user input
-  var userInput = document.querySelector('.user-input');  
-  var userInputValue = userInput.value;
+function displayTodo(){
+  const userInput = document.querySelector('.user-input');  
+  const userInputValue = userInput.value;
 
   if (userInputValue == ''){
-    alert('enter a todo pls!');
-  } else {
+    alert('Enter a todo please!!!');
+    return;
+  } 
 
-    // create li 
-    var li = document.createElement('li');
-    li.className = 'list-item';
-    li.textContent = userInputValue;
+  const todo = createTodo(userInputValue);
+  const deleteButton = createDeleteButton(todo);
 
-    // create delete btn
-    var deleteBtn = document.createElement('button');
-    deleteBtn.className = 'delBtn';
-    deleteBtn.textContent = 'X';
-
-    // append delete button to li & append li to ul
-    li.appendChild(deleteBtn);
-    list.appendChild(li);
-
-    var items = document.querySelectorAll('.list-item');
-
-    // Every time an item is added to the list, loop through the ul element
-    // and add the even listener to it  
-    items.forEach((item) => {
-      item.addEventListener('click', addStrikethrough);
-    })
-
-
-    // Add event listener to all Delete buttons
-    var delBtns = document.querySelectorAll('.delBtn');
-    
-    // Every time an item is added to the list, loop through the  element
-    // and add the even listener to it  
-    delBtns.forEach((btn) => {
-      btn.addEventListener('click', removeTodo);
-    })
-
-    // Reset input field to nothing
-    userInput.value = '';
-  }
+  todo.appendChild(deleteButton);
+  document.querySelector('.items').appendChild(todo);
+  
+  userInput.value = '';
 }
 
-// ----- DELETE TODO ----- 
-function removeTodo(e){
-  
-  // Grab reference to the ul
-  var list = document.querySelector('.items');
-  
-  // Grab reference to delBtn's parent element which is the li
-  var listItem = e.target.parentElement;
 
-  // Validation for removing an li
-  if (listItem.classList.contains('strikeThrough')){
-    list.removeChild(listItem);
+function createTodo(todoText){
+  const todo = document.createElement('li');
+  todo.className = 'list-item';
+  todo.textContent = todoText;
+  todo.addEventListener('click', addStrikethrough);
+  return todo;
+}
+
+
+function createDeleteButton(todo){
+  const deleteButton = document.createElement('button');
+  deleteButton.className = 'del-btn';
+  deleteButton.textContent = 'X';
+  deleteButton.addEventListener('click', () => {
+    removeTodo(todo);
+  });
+  return deleteButton;
+}
+
+
+function removeTodo(todo){
+  if (todo.classList.contains('strikeThrough')){
+    todo.remove();
   } else {
     alert('Complete todo first!');
   }
 }
 
 
-// ----- MARK TODO AS COMPLETE ----- 
-// sets the style of the target element to strike-through
 function addStrikethrough(e){
-  // Grab reference to li
-  var item = e.target;
+  const item = e.target;
 
-  // // if event is a li, then toggle on/off the strikeThrough class
   if (item.classList.contains('list-item')){
     item.classList.toggle('strikeThrough');
   }
 
 }
 
-// ----- CLEAR ALL TODOS -----
-function clearAllTodos(e){
 
-  var list = document.querySelector('.items');
-  var listItems = document.querySelectorAll('.list-item');
-
-  // Clears all lis from ul
-  listItems.forEach((item) => {
-    list.removeChild(item);
-  })
-
+function clearTodos(){
+  document.querySelector('.items').textContent = '';
 }
